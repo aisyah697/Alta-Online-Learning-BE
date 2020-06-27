d51c10e069ed87d8cd86bdf0f63338087f95726c
Yopi Ragil <yopi.ragil.yrpp@gmail.com>
2020-06-27 19:21:19 +0700

    push deploy dev

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh-add -k ~/.ssh/id_rsa &&
 
 source ~/.profile
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-docker stop flaskdemo
-docker rm flaskdemo
+docker stop be_alta
+docker rm be_alta
 docker rmi yopiragil/alta:latest
-docker run -d --name flaskdemo -p 5000:5000 yopiragil/alta:latest
+docker run -d --name be_alta -p 5000:5000 yopiragil/alta:latest
