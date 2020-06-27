7bb0a1cd55ca3c6cd3ac22a28b3e0b05c9e2f65b
Yopi Ragil <yopi.ragil.yrpp@gmail.com>
2020-06-27 20:57:06 +0700

    push deploy dev

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh-add -k ~/.ssh/id_rsa &&
 
 source ~/.profile
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-docker stop be_alta
-docker rm be_alta
-docker rmi yopiragil/alta:latest
-docker run -d --name be_alta -p 5000:5000 yopiragil/alta:latest
+sudo docker stop be_alta
+sudo docker rm be_alta
+sudo docker rmi yopiragil/alta:latest
+sudo docker run -d --name be_alta -p 5000:5000 yopiragil/alta:latest
